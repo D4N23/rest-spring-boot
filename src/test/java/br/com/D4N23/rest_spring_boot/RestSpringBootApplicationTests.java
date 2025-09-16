b69e78e72aafdbee1f89c9db9dc7417fb8591921
Danilo <daniilosam23@gmail.com>
2025-09-15 22:43:36 -0300

    initial commit

---
+++ src/test/java/br/com/D4N23/rest_spring_boot/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.D4N23.rest_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
